e44f3beed0364abeb8e8d3b2f7a905bf47a5244e
thiagopazBR <thiagopaz_rj@hotmail.com>
2022-07-09 20:58:43 +0000

    commit

---
+++ src/functions/prepare_query.ts
@@ -0,0 +1,79 @@
+import { ICsvData } from '../interfaces/ICsvData'
+
+const prepare_query = (data: ICsvData): string => {
+  const output = `INSERT INTO ${process.env.COMMISSIONING_REPORT_TABLE} (
+      DATE_,
+      DEVICE_ID,
+      DEVICE_MODEL,
+      DEVICE_TYPE,
+      IP_ADDR,
+      FIRMWARE_VERSION,
+      METER_VERSION,
+      GATEWAY_ID,
+      GATEWAY_IP,
+      PARENT,
+      RSSI,
+      LQI,
+      DEVICE_STATE,
+      ONLINE_RATE,
+      TOTAL_OFFLINE_TIME,
+      REGISTRATION_TIME,
+      ROUTE_LEVEL,
+      LAST_COMMMUNICATION_TIME,
+      LAST_DATA_TIME,
+      ACTIVE_ENERGY_PLUS_SIGN_TOTAL_DAILY,
+      ACTIVE_ENERGY_PLUS_SIGN_TARIFF1_DAILY,
+      ACTIVE_ENERGY_PLUS_SIGN_TARIFF2_DAILY,
+      ACTIVE_ENERGY_PLUS_SIGN_TARIFF3_DAILY,
+      ACTIVE_ENERGY_PLUS_SIGN_TARIFF4_DAILY,
+      ACTIVE_ENERGY_MINUS_SIGN_TOTAL_DAILY,
+      ACTIVE_ENERGY_MINUS_SIGN_TARIFF1_DAILY,
+      ACTIVE_ENERGY_MINUS_SIGN_TARIFF2_DAILY,
+      ACTIVE_ENERGY_MINUS_SIGN_TARIFF3_DAILY,
+      ACTIVE_ENERGY_MINUS_SIGN,
+      FIRST_COMMUNICATION_TIME
+    )
+
+    values (
+      ${quotation(data['Date'])},
+      ${quotation(data['Device ID'])},
+      ${quotation(data['Device Model'])},
+      ${quotation(data['Device Type'])},
+      ${quotation(data['IP'])},
+      ${quotation(data['Firmware Version'])},
+      ${quotation(data['Meter Version'])},
+      ${quotation(data['Gateway ID'])},
+      ${quotation(data['Gateway IP'])},
+      ${quotation(data['Parent'])},
+      ${quotation(data['RSSI'])},
+      ${quotation(data['LQI'])},
+      ${quotation(data['Device State'])},
+      ${quotation(data['Online Rate'])},
+      ${quotation(data['Total Offline Time'])},
+      ${quotation(data['Registration Time'])},
+      ${quotation(data['Route Level'])},
+      ${quotation(data['Last Commmunication Time'])},
+      ${quotation(data['Last Data Time'])},
+      ${quotation(data['Active energy(+) total [Daily]'])},
+      ${quotation(data['Active energy (+) tariff1 [Daily]'])},
+      ${quotation(data['Active energy (+) tariff2 [Daily]'])},
+      ${quotation(data['Active energy (+) tariff3 [Daily]'])},
+      ${quotation(data['Active energy (+) tariff4 [Daily]'])},
+      ${quotation(data['Active energy(-) total [Daily]'])},
+      ${quotation(data['Active energy(-) tariff1 [Daily]'])},
+      ${quotation(data['Active energy(-) tariff2 [Daily]'])},
+      ${quotation(data['Active energy(-) tariff3 [Daily]'])},
+      ${quotation(data['Active energy(-)'])},
+      ${quotation(data['First Communication Time'])}
+    )
+  `
+
+  return output.replace(/(?:\r\n|\r|\n)/g, '')
+}
+
+const quotation = (x: string | undefined): string | undefined => {
+  if (x) return `'${x}'`
+  else return 'null'
+}
+
+export { prepare_query }
