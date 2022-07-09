32066340530aa672955df999e0839cbab7779483
thiagopazBR <thiagopaz_rj@hotmail.com>
2022-07-09 20:57:58 +0000

    commit

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,9 @@ import 'dotenv/config'
 import * as path from 'path'
 
 import { Mssql } from './classes/Mssql'
-import { data_validation_commissioning_report } from './functions/data_validation'
+import * as data_validation from './functions/data_validation'
 import { read_csv } from './functions/read_csv'
+import { prepare_query } from './functions/prepare_query'
 
 // const script_dir: string = path.dirname(__filename)
 const target_path = '/workspaces/hexing-azure-sql/files' // Dir where is commissioning_report.csv files
@@ -18,18 +19,19 @@ const mssql = new Mssql()
 
 ;(async () => {
   await mssql.init()
-  const r = await mssql.select()
-  r.recordset.forEach(v => console.log(v))
+  // const r = await mssql.select()
+  // r.recordset.forEach(v => console.log(v))
 
   const csv_content = await read_csv(csv_file_path)
   let i = csv_content.length
   while (i--) {
     let row = csv_content[i]
-    row = data_validation_commissioning_report(row)
+    row = data_validation['data_validation_commissioning_report'](row)
     // const device_type: string = row['Device Type']
     // const relay_orca_serial: string = row['Device ID']
     // const last_communication_time: string = row['Last Commmunication Time']
-
-    console.log(row)
+    const query: string = prepare_query(row)
+    await mssql.select(query)
+    console.log(row['Device ID'])
   }
 })()
