01c0844cd93d85ae191983131998a9ecdc656271
thiagopazBR <thiagopaz_rj@hotmail.com>
2022-07-12 05:18:03 +0000

    commit

--- src/index.ts
+++ src/index.ts
@@ -36,21 +36,34 @@ const date_range = date_validation.generate_date_range(start_date, end_date)
 
   for (const date of date_range) {
     console.log(date)
-    const csv_file_path = path.join(target_path, `${date}_commissioning_report.csv`)
+    const csv_file_path = path.join(target_path, `${date}_${target_script}.csv`)
 
     if (!existsSync(csv_file_path)) {
-      console.log(`${date}_commissioning_report.csv file does not exists`)
+      console.log(`${date}_${target_script}.csv file does not exists`)
       continue
     }
 
     const csv_content = await read_csv(csv_file_path)
 
-    const create_table = prepare_create_table[target_script](date)
-    await mssql.select(create_table)
+    // eslint-disable-next-line  no-constant-condition
+    if (0 > 1) {
+      // Yes, I know. I'm forcing to ignorer it
+      const create_table = prepare_create_table[target_script](date)
+      await mssql.select(create_table)
+    }
+
+    const device_ids: { [key: string]: boolean } = {}
 
     let i = csv_content.length
     while (i--) {
       let row = csv_content[i]
+
+      if (target_script == 'commissioning_report') {
+        const output_device_id: string = check_device_id(row['Device ID'])
+        if (output_device_id == 'error') continue
+        if (device_ids[output_device_id] !== undefined) continue
+        device_ids[output_device_id] = true
+      }
       row = csv_validation[target_script](row)
       const query: string = prepare_query(date, row)
       await mssql.select(query)
@@ -60,3 +73,16 @@ const date_range = date_validation.generate_date_range(start_date, end_date)
 })()
 // const r = await mssql.select()
 // r.recordset.forEach(v => console.log(v))
+
+const check_device_id = (device_id: string): string => {
+  if (!device_id.match(/^[0-9]+$/)) return 'error'
+
+  if (device_id.length < 8) return 'error'
+
+  const d: number = parseInt(device_id)
+  if (d < -2147483648 || d > 2147483647) return 'error'
+
+  if (device_id.startsWith('0000')) while (device_id.charAt(0) === '0') device_id = device_id.slice(1)
+
+  return device_id
+}
